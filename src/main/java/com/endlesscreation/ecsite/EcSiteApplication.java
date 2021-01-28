44cbaab93a9d4c926b812f4dd02361e9446a751c
김예림 <kiel0103@naver.com>
2021-01-28 17:39:37 +0900

    빈 프로젝트 생성

---
+++ src/main/java/com/endlesscreation/ecsite/EcSiteApplication.java
@@ -0,0 +1,12 @@
+package com.endlesscreation.ecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcSiteApplication.class, args);
+    }
+}
